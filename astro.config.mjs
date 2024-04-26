9760f4429fbb82ae3dd47c699d0e72cc181661eb
citenote <archaeology.cn@outlook.com>
2024-04-26 23:05:15 +0800

    added new content

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,24 +1,30 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
+import partytown from "@astrojs/partytown";
+import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
-	site: 'https://refcite.github.io',
-	base: '/',
-	integrations: [
-		starlight({
-			title: '文獻',
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						{ label: 'Example Guide', link: '/guides/example/' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
+  site: 'https://refcite.github.io',
+  base: '/',
+  integrations: [starlight({
+    title: '文獻',
+    locales: {
+      root: {
+        label: '繁體中文',
+        lang: 'zh-TW'
+      }
+    },
+    sidebar: [{
+      label: '古文字學',
+      items: [{
+        label: '甲骨文',
+        link: '/paleography/oracle-bone'
+      }, {
+        label: '金文',
+        link: '/paleography/bronze-inscription'
+      }]
+    }]
+  }), 
+  partytown(), 
+  tailwind()]
 });
