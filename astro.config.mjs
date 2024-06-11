99dfba1471c118cf5c6125bc31a5e5d0f1ef9f7f
citenote <archaeology.cn@outlook.com>
2024-06-11 13:07:25 +0800

    first deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,30 +1,11 @@
 import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
-import partytown from "@astrojs/partytown";
-import tailwind from "@astrojs/tailwind";
+import mdx from '@astrojs/mdx';
 
+import sitemap from '@astrojs/sitemap';
+
+// https://astro.build/config
 export default defineConfig({
-  site: 'https://refcite.github.io',
-  base: '/',
-  integrations: [starlight({
-    title: '文獻',
-    locales: {
-      root: {
-        label: '繁體中文',
-        lang: 'zh-TW'
-      }
-    },
-    sidebar: [{
-      label: '古文字學',
-      items: [{
-        label: '甲骨文',
-        link: '/paleography/oracle-bone'
-      }, {
-        label: '金文',
-        link: '/paleography/bronze-inscription'
-      }]
-    }]
-  }), 
-  partytown(), 
-  tailwind()]
+	site: 'https://arcnote.github.io',
+	base: '/',
+	integrations: [mdx(), sitemap()],
 });
